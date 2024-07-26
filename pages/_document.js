2812ff0a04856393df391d778052d156996991f8
Pratikshresth <58677787+Pratikshresth@users.noreply.github.com>
2024-07-25 22:42:47 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,17 @@
 // pages/_document.js
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>Pratik Shrestha</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
