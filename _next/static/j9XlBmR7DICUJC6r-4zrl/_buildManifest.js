ae08637e43c19d41ac3ffe127e0ce77e4fd3f300
Pratikshresth <Pratikshresth@users.noreply.github.com>
2024-07-30 21:19:18 +0000

    deploy: f9b46a42687fab96a6919b319b46ce4ff2760857

--- _next/static/j9XlBmR7DICUJC6r-4zrl/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/689-97caebfa90d4f78f.js","static/chunks/pages/index-971210e6aaa2ee10.js"],"/_error":["static/chunks/pages/_error-1be831200e60c5c0.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
